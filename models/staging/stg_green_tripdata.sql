ec13bae5c674cec8d53b7022ab645e46f3c097da
Andrew Katoch <andrewvk7@gmail.com>
2023-02-23 02:02:05 +0000

    Finished models and testing - ready to deploy

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,5 +1,12 @@
 {{ config(materialized="view") }}
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','external_green_tripdata') }}
+  where vendorid is not null 
+)
 select
     -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
@@ -31,7 +38,10 @@ select
     {{ get_payment_type_description('payment_type') }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
-from {{ source("staging", "external_green_tripdata") }}
+from tripdata
+where rn = 1
+
+
 
 where vendorid is not null
 {% if var('is_test_run', default=true) %}
